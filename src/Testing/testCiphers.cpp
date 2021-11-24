24e59411b9831d83b743ff6b23ff4fc5654d7fa8
Katharina Lachner <queenofmice41@gmail.com>
2021-11-24 17:56:01 +0000

    Exercise 7 - test all ciphers from a vector
    can now add more ciphers and corresponding test strings to testCiphers
    by simply pushing them back to the existing vectors

--- src/Testing/testCiphers.cpp
+++ src/Testing/testCiphers.cpp
@@ -1,10 +1,12 @@
 #define CATCH_CONFIG_MAIN
 
+#include <cassert>
 #include <vector> 
 
 #include "catch.hpp"
 #include "Cipher.hpp"
 #include "CipherFactory.hpp"
+#include "CipherType.hpp"
 #include "CaesarCipher.hpp"
 #include "PlayfairCipher.hpp"
 #include "VigenereCipher.hpp"
@@ -18,28 +20,47 @@ bool testCipher( const Cipher& cipher, const CipherMode mode, const std::string&
 
 }
 
-std::vector<std::unique_ptr<Cipher>> inventory = new std::vector<std::unique_ptr<Cipher>>;
-inventory->push_back( cipherFactory( CipherTypes::Caesar, 10 ) );
-inventory->push_back( cipherFactory( CipherTypes::Playfair, "hello" ) );
-inventory->push_back( cipherFactory( CipherTypes::Vigenere, "abc" ) );
+TEST_CASE("test case for de and encryption of various cipher types") {
 
-std::vector<std::string> in;
-in->push_back("HELLOWORLD");
-in->push_back("BOBISSOMESORTOFJUNIORCOMPLEXXENOPHONEONEZEROTHING");
-in->push_back("ONETESST");
+    std::vector<std::unique_ptr<Cipher>> inventory;
+    inventory.push_back(cipherFactory( CipherType::Caesar, "10" ));
+    inventory.push_back(cipherFactory( CipherType::Playfair, "hello" ));
+    inventory.push_back(cipherFactory( CipherType::Vigenere, "abc" ));
 
-std::vector<std::string> out;
-out->push_back("ROVVYGYBVN");
-out->push_back("FHIQXLTLKLTLSUFNPQPKETFENIOLVSWLTFIAFTLAKOWATEQOKPPA");
-out->push_back("OOGTFUSU");
+    std::vector<std::pair<std::string, std::string>> teststrings; 
+    teststrings.push_back(std::make_pair("HELLOWORLD", "ROVVYGYBVN"));
+    teststrings.push_back(std::make_pair("BOBISSOMESORTOFJUNIORCOMPLEXXENOPHONEONEZEROTHING", "FHIQXLTLKLTLSUFNPQPKETFENIOLVSWLTFIAFTLAKOWATEQOKPPA"));
+    teststrings.push_back(std::make_pair("ONETESST", "OOGTFUSU"));
 
-std::cassert (inventory.size() == in.size());
-std::cassert (out.size() == in.size());
 
-for (int i = 0; i < inventory.size(), i++) {
-    TEST_CASE("test case") {
-        REQUIRE (testCipher(inventory->at(i), CipherMode::Encrypt, in->at(i), out->.at(i)));
-        REQUIRE (testCipher(inventory->at(i), CipherMode::Decrypt, out->.at(i), in->.at(i)));
+    assert(teststrings.size() == inventory.size());
+
+    for (std::size_t i = 0; i < inventory.size(); i++) {
+        REQUIRE(testCipher(*inventory.at(i), CipherMode::Encrypt, teststrings.at(i).first, teststrings.at(i).second));
+        REQUIRE(testCipher(*inventory.at(i), CipherMode::Decrypt, teststrings.at(i).second, teststrings.at(i).first));
     }
 
 }
+// ?) version 2 - why does the following implementation not work..?
+// create a map of cipher, input string and output string
+
+/*
+TEST_CASE("test case for de and encryption of various cipher types - version 2") {
+    std::map <std::unique_ptr<Cipher>, std::pair<std::string, std::string>> inventory { 
+        {cipherFactory( CipherType::Caesar, "10" ), std::make_pair ("HELLOWORLD", "ROVVYGYBVN")},
+        {cipherFactory( CipherType::Playfair, "hello" ), std::make_pair ("BOBISSOMESORTOFJUNIORCOMPLEXXENOPHONEONEZEROTHING", "FHIQXLTLKLTLSUFNPQPKETFENIOLVSWLTFIAFTLAKOWATEQOKPPA")},
+        {cipherFactory( CipherType::Vigenere, "abc" ), std::make_pair ("ONETESST", "OOGTFUSU")},
+    };
+
+    for (std::map <std::unique_ptr<Cipher>, std::pair<std::string, std::string>>::iterator it = inventory.begin(); it != inventory.end(); it++) {
+        auto cc = it->first;
+        auto val = it->second;
+        std::string in = val.first;  
+        std::string out = val.second;  
+
+        // run tests
+        REQUIRE (testCipher(it.first, CipherMode::Encrypt, in, out));
+        REQUIRE (testCipher(it.first, CipherMode::Decrypt, out, in));
+    }
+}
+*/
