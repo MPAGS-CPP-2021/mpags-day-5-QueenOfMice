b05fc7f47bc7c3ceed853e08ee612c043e07d08e
Katharina Lachner <queenofmice41@gmail.com>
2021-11-24 15:51:11 +0000

    intermediate commit - not working right now

--- src/Testing/testCiphers.cpp
+++ src/Testing/testCiphers.cpp
@@ -1,7 +1,10 @@
 #define CATCH_CONFIG_MAIN
 
+#include <vector> 
+
 #include "catch.hpp"
 #include "Cipher.hpp"
+#include "CipherFactory.hpp"
 #include "CaesarCipher.hpp"
 #include "PlayfairCipher.hpp"
 #include "VigenereCipher.hpp"
@@ -15,27 +18,28 @@ bool testCipher( const Cipher& cipher, const CipherMode mode, const std::string&
 
 }
 
+std::vector<std::unique_ptr<Cipher>> inventory = new std::vector<std::unique_ptr<Cipher>>;
+inventory->push_back( cipherFactory( CipherTypes::Caesar, 10 ) );
+inventory->push_back( cipherFactory( CipherTypes::Playfair, "hello" ) );
+inventory->push_back( cipherFactory( CipherTypes::Vigenere, "abc" ) );
 
-TEST_CASE("Caesar Cipher encryption and decryption", "[caesar]") {
-  CaesarCipher cc{10};
-  const std::string inCae = "HELLOWORLD";
-  const std::string outCae = "ROVVYGYBVN";
-  REQUIRE (testCipher(cc, CipherMode::Encrypt, inCae, outCae));
-  REQUIRE (testCipher(cc, CipherMode::Decrypt, outCae, inCae));
-}
+std::vector<std::string> in;
+in->push_back("HELLOWORLD");
+in->push_back("BOBISSOMESORTOFJUNIORCOMPLEXXENOPHONEONEZEROTHING");
+in->push_back("ONETESST");
 
-TEST_CASE("Playfair Cipher encryption and decryption", "[playfair]") {
-  PlayfairCipher cc{"hello"};
-  const std::string inPlayf = "BOBISSOMESORTOFJUNIORCOMPLEXXENOPHONEONEZEROTHING";
-  const std::string outPlayf = "FHIQXLTLKLTLSUFNPQPKETFENIOLVSWLTFIAFTLAKOWATEQOKPPA";
-  REQUIRE (testCipher(cc, CipherMode::Encrypt, inPlayf, outPlayf));
-  REQUIRE (testCipher(cc, CipherMode::Decrypt, outPlayf, inPlayf));
-}
+std::vector<std::string> out;
+out->push_back("ROVVYGYBVN");
+out->push_back("FHIQXLTLKLTLSUFNPQPKETFENIOLVSWLTFIAFTLAKOWATEQOKPPA");
+out->push_back("OOGTFUSU");
 
-TEST_CASE("Vigenere Cipher encryption and decryption", "[vigenere]") {
-  VigenereCipher cc{"abc"};
-  const std::string inVig = "1Te!sSt;";
-  const std::string outVig = "OOGTFUSU";
-  REQUIRE (testCipher(cc, CipherMode::Encrypt, inVig, outVig));
-  REQUIRE (testCipher(cc, CipherMode::Decrypt, outVig, "ONETESST"));
+std::cassert (inventory.size() == in.size());
+std::cassert (out.size() == in.size());
+
+for (int i = 0; i < inventory.size(), i++) {
+    TEST_CASE("test case") {
+        REQUIRE (testCipher(inventory->at(i), CipherMode::Encrypt, in->at(i), out->.at(i)));
+        REQUIRE (testCipher(inventory->at(i), CipherMode::Decrypt, out->.at(i), in->.at(i)));
+    }
+
 }
